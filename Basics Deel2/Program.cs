d5608c438df9eec098646b6c932111a04862d923
Olli Appelt <30849@ma-web.nl>
2021-11-05 09:07:56 +0000

    basics deel2

---
+++ Basics Deel2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        
+    }
+    public class Rectangle
+    {
+        public string = "bird";
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+        private bool Square = true;
+
+        public Rectangle Rechthoek(int x, int y, int width, int height)
+        {
+            int x = 100;
+            int y = 100;
+            int width = 50;
+            int height = 60;
+        }
+
+        public Rectangle Vierkant(int x, int y, int size)
+        {
+            int x = 50;
+            int y = 50;
+            size = 50;
+        }
+
+        public Rectangle vierkantBeweeg(int width, int height)
+        {
+            size = 50;
+        }
+
+        public Rectangle rechthoekBeweeg(int Size)
+        {
+            width = 20;
+            height = 30;
+        }
+
+        public void MoveTo(int x, int y)
+        {
+
+        }
+
+        public void IsSquare()
+        {
+            if (Square = true)
+            {
+                Console.WriteLine("Vierkant");
+            }
+        }
+        public void Set(int x, int y, int width, int height)
+        {
+
+        }
+    }
+}
