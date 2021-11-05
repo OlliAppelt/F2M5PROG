acd207ad43c83ea0dedd29eb723b760997611d54
Olli Appelt <30849@ma-web.nl>
2021-11-05 09:43:28 +0000

    Inheritance

---
+++ Les5 - Inheritance/SpriteInheritance/SirKibblesFriend.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Eindopdracht
+{
+	class SirkibblesFriend : Sprite
+	{
+		public SirkibblesFriend(RectangleF screenPosition)
+        {
+			this.screenPosition = screenPosition;
+			imageFrame = new Rectangle(35, 120, 16, 16);
+        }
+	}
+}
