17db5e8b92535803a764d0b963f0435ddb7a5aca
Olli Appelt <30849@ma-web.nl>
2021-11-05 09:22:48 +0000

    Classes

---
+++ Les2 - Classes/Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Enemy
+    {
+        public int Health = 100;
+        public int currentHealth;
+        public bool attackPlayer;
+        public bool enemyAttacked;
+
+        void takeDamage()
+        {
+            if (enemyAttacked == true)
+            {
+                Health =- 10;
+            }
+        }
+    }
+}
